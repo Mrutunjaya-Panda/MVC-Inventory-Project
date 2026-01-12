58ed5853441a3018bcd4e39696bf7f13767b8ed2
Mrutunjaya Panda <mrutunjayapanda28@gmail.com>
2026-01-13 00:11:39 +0530

    a model file is added with some modifications in the controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,9 +1,12 @@
 //here in this controller folder we will handle all the product related requests and will
 //serve the product files dynamically
 import path from 'path';
+import ProductModel from '../models/product.model.js';
 
 export default class ProductController{
     getProducts(req,res){
+        let products = ProductModel.get();
+        console.log(products);
         return res.sendFile(path.join(process.cwd(),'src','views','products.html'));
         //or
         //return res.sendFile(path.resolve('src','views','products.html'));
