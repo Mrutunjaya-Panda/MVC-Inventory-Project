17b1b203e9a0024ba6a558dd71da881398d19edb
Mrutunjaya Panda <mrutunjayapanda28@gmail.com>
2026-01-14 00:51:08 +0530

    view engine part done inorder to render files dynamically w/o hardcoding it.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 //const express = require('express');
 import express from 'express';
 import ProductController from './src/controllers/product.controller.js';
+import Path from 'path';
 
 const server = express();
 
 // server.get('/',(req,res) => {
 //     return res.send('Welcome to Inventory App');
 // })
+
+//setup view engine settings
+server.set('view engine', 'ejs'); //set() is used to set some settings in express app
+
+//next we have to set the route where ejs will find the views folder
+server.set('views', Path.join(process.cwd(), 'src','views')); //by default it looks for views folder in root directory
+//server.set('views', Path.join(path.resolve() 'src','views'));
 
 const productController = new ProductController();
 
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -7,8 +7,15 @@ export default class ProductController{
     getProducts(req,res){
         let products = ProductModel.get();
         console.log(products);
-        return res.sendFile(path.join(process.cwd(),'src','views','products.html'));
+        //we are not going to use below line it anymore after ejs addition
+        //return res.sendFile(path.join(process.cwd(),'src','views','products.html'));
         //or
         //return res.sendFile(path.resolve('src','views','products.html'));
+
+        //the render() function takes parameters view name(i.e ejs file name) and data to be sent to view
+        //data is sent in form of object
+        return res.render('products', {products: products});
+
+        //N.B:- The key name in the object i.e 'products' should be same as used in ejs file to access the data
     }
 }
