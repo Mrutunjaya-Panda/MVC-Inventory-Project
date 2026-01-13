2bcb928d8ee779227fb8b2f8c0f49d6c66968982
Mrutunjaya Panda <mrutunjayapanda28@gmail.com>
2026-01-14 02:30:49 +0530

    layout structure done for reusable purposes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import ProductController from './src/controllers/product.controller.js';
 import Path from 'path';
+import ejsLayouts from 'express-ejs-layouts';
 
 const server = express();
 
@@ -15,6 +16,9 @@ server.set('view engine', 'ejs'); //set() is used to set some settings in expres
 //next we have to set the route where ejs will find the views folder
 server.set('views', Path.join(process.cwd(), 'src','views')); //by default it looks for views folder in root directory
 //server.set('views', Path.join(path.resolve() 'src','views'));
+
+//after setup of view engine we have to use ejs layouts middleware
+server.use(ejsLayouts);//this will help to use layouts in ejs files
 
 const productController = new ProductController();
 
